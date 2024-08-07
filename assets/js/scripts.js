e2325ec975dced0f3be93cf4734591db252d93ad
KillMonga130 <mubvafhimoses813@gmail.com>
2024-08-07 12:48:31 +0200

    finishing touches

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,16 +1,9 @@
 // script.js
 
-document.querySelector('.info_more-btn').addEventListener('click', function() {
-    document.querySelector('.sidebar-info_more').classList.toggle('active');
-});
-
-// Smooth scrolling for navigation links
-document.querySelectorAll('.navbar-link').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
+document.querySelectorAll('.navbar .nav-list li a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
         e.preventDefault();
-        document.querySelectorAll('.navbar-link').forEach(link => link.classList.remove('active'));
-        this.classList.add('active');
-        document.querySelector(this.getAttribute('data-nav-link')).scrollIntoView({
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
         });
     });
