e817867eafbd1c1c2b99f375b5fdaffb1fcc3beb
KillMonga130 <mubvafhimoses813@gmail.com>
2024-09-04 16:27:28 +0200

    moses

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,4 +45,18 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Start typing effect immediately
     typeText();
+
+
+    const form = document.querySelector('.contact-form');
+    form.addEventListener('submit', function(event) {
+    const name = document.querySelector('#name').value;
+    const email = document.querySelector('#email').value;
+    const message = document.querySelector('#message').value;
+    
+    if (name === '' || email === '' || message === '') {
+        alert('Please fill in all fields');
+        event.preventDefault();
+    }
+});
+
 });
